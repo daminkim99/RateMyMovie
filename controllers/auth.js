31f6e92a957bf19b47463b8b8f7a9354237fa0a9
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-04-12 19:44:54 -0300

    fix logout error cannot read properties of undefined

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,9 +45,9 @@ exports.getLogin = (req, res) => {
   };
   
   exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
+    // req.logout(() => {
+    //   console.log('User has logged out.')
+    // }) --> logout should be post not a get request 
     req.session.destroy((err) => {
       if (err)
         console.log("Error : Failed to destroy the session during logout.", err);
